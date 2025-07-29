b891e59308dab818df0701dc0dfa27ef1749eb5d
Amaia <amaiavzqz@gmail.com>
2025-07-29 20:10:03 +0200

    pp tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
+import { CustomCard } from "./components/card/CustomCard";
+import { Container } from "./components/container/Container";
+
 function App() {
   return (
     <>
       <main>
-        <h1 className="text-4xl font-bold text-blue-950">Tasks</h1>
+        <section className="py-16">
+          <Container>
+            <h1 className="text-4xl font-bold text-blue-950">Tasks</h1>
+          </Container>
+        </section>
 
-        <article className="w-full max-w-2xl bg-gray-400 rounded-2xl overflow-hidden">
-          <img src="/img01.png" alt="" />
-          <div className="p-4">
-            <h2 className="text-2xl font-bold text-(--title-color)">Titulo de la card</h2>
-            <p className="text-gray-100">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti fugiat omnis, cupiditate obcaecati saepe provident exercitationem, deleniti vel nesciunt, beatae possimus consequatur? Quod, placeat nihil adipisci necessitatibus vitae voluptatum fugit optio, suscipit quidem soluta laboriosam quaerat facilis, tempore aperiam. Est qui pariatur hic! Culpa omnis quasi amet eos, veritatis autem.</p>
-            <a href="#">Leer mas</a>
-          </div>
-        </article>
+        <section className="bg-purple-300 py-12">
+          <Container>
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-4">
+              {[1, 2, 3, 4].slice().map((card, i) => (
+                <CustomCard key={i} />
+              ))}
+            </div>
+          </Container>
+        </section>
       </main>
     </>
   );
---
+++ src/components/container/Container.jsx
@@ -0,0 +1,6 @@
+
+export const Container = ({children}) => {
+  return (
+    <div className="max-w-7xl mx-auto w-full px-4">{children}</div>
+  )
+}
