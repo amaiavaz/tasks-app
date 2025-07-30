effc6dc4fcd774d0150ff01d27c6ea687f53dc1d
Amaia <amaiavzqz@gmail.com>
2025-07-30 13:56:10 +0200

    estilos header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { Container } from "../container/Container";
 
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="py-4">
+      <Container>
+        <div className="flex justify-between align-middle">
+          <h1 className="text-2xl font-bold">MyTasks</h1>
+          <div>
+            <button 
+              className="btn"
+            >+Add</button>
+          </div>
+        </div>
+        <div className="divider"></div>
+      </Container>
+    </header>
+  );
+};
