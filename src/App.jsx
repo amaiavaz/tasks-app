ea75ad005ab94bbba4cc1360bf195d3cc27cfc43
Amaia <amaiavzqz@gmail.com>
2025-07-30 12:55:49 +0200

    estructura basica

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
-import { CustomCard } from "./components/card/CustomCard";
-import { Container } from "./components/container/Container";
+import { Footer } from "./components/footer/Footer";
+import { Header } from "./components/header/Header";
+import { Main } from "./components/main/Main";
 
 function App() {
+
   return (
     <>
-      <main>
-        <section className="py-16">
-          <Container>
-            <h1 className="text-4xl font-bold text-blue-950">Tasks</h1>
-          </Container>
-        </section>
-
-        <section className="bg-purple-300 py-12">
-          <Container>
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-4">
-              {[1, 2, 3, 4].slice().map((card, i) => (
-                <CustomCard key={i} />
-              ))}
-            </div>
-          </Container>
-        </section>
-      </main>
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
---
+++ src/components/subtaskList/SubtaskList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const SubtaskList = () => {
+  return (
+    <div>SubtaskList</div>
+  )
+}
