c66e85ec4dbb831b9eb26a690c0ae949d30cf6cf
Amaia <amaiavzqz@gmail.com>
2025-07-31 12:02:29 +0200

    mapeo tasks

--- src/components/taskList/TaskList.jsx
+++ src/components/taskList/TaskList.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
 
-export const TaskList = ({tasks}) => {
+
+export const TaskList = ({ tasks }) => {
   return (
     <div>
-      <ul className='space-y-6'>
-        <li className="w-full max-w-140 mx-auto">
-          <div className='w-full text-[1.25rem] bg-indigo-100 px-1.5 py-2.5 border-3 border-indigo-900 rounded-3xl'>
-            Task 1
-          </div>
-        </li>
+      <ul className="space-y-6">
+        {tasks?.map((elem, i) => {
+          return(
+          <li key={i} className="mx-auto w-full max-w-140">
+            <div className="flex w-full items-center justify-between rounded-3xl border-3 border-indigo-900 bg-indigo-100 px-6 py-2.5">
+              <button className="text-[1.25rem] font-semibold">
+                {elem}
+              </button>
+              <div className="flex justify-center gap-8">
+                <p>0.5%</p>
+                <p className="font-semibold">1</p>
+                <button>
+                  <img src="/detele.svg" alt="delete" />
+                </button>
+              </div>
+            </div>
+          </li>
+          )
+        })}
       </ul>
     </div>
-  )
-}
+  );
+};
