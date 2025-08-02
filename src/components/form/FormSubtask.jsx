bed0bf6f1ae61aa9ec3d5bc892c27c7cd12470c9
Amaia <amaiavzqz@gmail.com>
2025-08-02 12:46:03 +0200

    act estilos form

--- src/components/form/FormSubtask.jsx
+++ src/components/form/FormSubtask.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const FormSubtask = () => {
   return (
     <form className='mx-auto space-y-5 p-6 bg-indigo-300 w-full max-w-110 rounded-3xl'>
-      <div className='flex flex-col items-center gap-1.5 text-[1.10rem]'>
+      <div className='flex flex-col items-center gap-1.5 text-[1.15rem]'>
         <label htmlFor="newItem" className='font-bold'>Add an item</label>
         <input
           id='newItem'
@@ -11,16 +11,16 @@ export const FormSubtask = () => {
           type="text" 
         />
       </div>
-      <div className='flex flex-col items-center gap-1 text-[1.10rem]'>
+      <div className='flex flex-col items-center gap-1 text-[1.15rem]'>
         <label htmlFor="quantity" className='font-bold'>Quantity</label>
         <div className='space-x-4'>
-          <button className='font-bold text-2xl'>+</button>
+          <button className='font-bold text-3xl'>+</button>
           <input
             id='quantity'
             className='bg-amber-50 w-10 rounded-2xl'
             type="text" 
           />
-          <button className='font-bold text-2xl'>-</button>
+          <button className='font-bold text-3xl'>-</button>
         </div>
       </div>
       <div className='flex justify-between px-8'>
